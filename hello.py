c0f581e9277e4dc2a4b7fe5aa0ad5cee41916837
Akim Ruslanov <akim2602@students.cs.ubc.ca>
2019-12-24 21:07:52 -0800

    last commit from PC

--- hello.py
+++ hello.py
@@ -1,5 +1,15 @@
 import pyperclip
 import sys
+def printingNumbers(num):
+    try:
+        print(int(num))
+    except ValueError:
+        print("Can't print text")
+
+
 
 pyperclip.copy("Hello world")
 print(pyperclip.paste())
+print('He', end='')
+print('man')
+printingNumbers("t")
